d5839bcd26392fa92da35638c96bf452a9b397cd
brown <86018533+2brownc@users.noreply.github.com>
2023-09-01 11:32:28 +0530

    find ip

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,7 +2,31 @@ import { CountButton } from "~features/count-button"
 
 import "~style.css"
 
+
+
 function IndexPopup() {
+  // api
+  // ipify
+  const IPIFY_API_URL: string = 'https://api.ipify.org?format=json'
+  
+  // state
+  const [errorMsg, setErrorMsg] = useState<string | any>(null)
+  const [IP, setIP] = useState<string | null>(null)
+  
+
+  // get IP address
+  const getIP = async () => {
+    const response = await fetch(IPIFY_API_URL, {method: 'GET'})
+
+    if(response.ok){
+      const { ip } = await response.json()
+      setIP(ip)
+    } else {
+      const errorMessage = `Failed to get IP: ${response.status}`
+      setErrorMsg(errorMessage)
+    }
+  }  
+  
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
       <CountButton />
