7037cb746ebb772a52ce5f5d59093e4345cd518f
brown <86018533+2brownc@users.noreply.github.com>
2023-09-01 11:44:34 +0530

    find loc

--- src/popup.tsx
+++ src/popup.tsx
@@ -9,10 +9,15 @@ function IndexPopup() {
   // ipify
   const IPIFY_API_URL: string = 'https://api.ipify.org?format=json'
   
+  //ipinfo
+  const IPINFO_API_URL: (arg0: string) => string = (ip) => 'https://ipinfo.io/${ip}'
+  const IPINFO_ACCESS_TOKEN = process.env.PLASMO_PUBLIC_IPINFO_ACCESS_TOKEN
+  
   // state
   const [errorMsg, setErrorMsg] = useState<string | any>(null)
   const [IP, setIP] = useState<string | null>(null)
-  
+  const [country, setCountry] = useState<string | null>(null)
+  const [city, setCity] = useState<string | null>(null)  
 
   // get IP address
   const getIP = async () => {
@@ -26,6 +31,24 @@ function IndexPopup() {
       setErrorMsg(errorMessage)
     }
   }  
+
+  // get location
+  const getLoc = async () => {
+    const headers = {
+      Authorization: `Bearer ${IPINFO_ACCESS_TOKEN}`,
+    }
+
+    const response = await fetch(IPINFO_API_URL(IP), { headers })
+
+    if(response.ok){
+      const {t_country, t_city} = await response.json()
+      setCountry(t_country)
+      setCity(t_city)
+    } else {
+      const errorMessage = `Failed to get IP location: ${response.status}`
+      setErrorMsg(errorMessage)
+    } 
+  }
   
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
